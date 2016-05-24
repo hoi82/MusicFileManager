bd682163f9ed334439bad3b8aee118fad30bd802
hoi82 <hoi0622@gmail.com>
2016-05-24 17:42:43 +0900

    5/24 Commit
    BackgroundWorker, DotNetZip

---
+++ FileRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicFileManager
+{
+    public class FileRepository
+    {
+        List<string> archivedFiles = new List<string>();
+        List<string> audioFiles = new List<string>();
+    }
+}
--- Main.xaml.cs
+++ Main.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.IO;
+using Ionic.Zip;
+using System.ComponentModel;
 
 namespace MusicFileManager
 {
@@ -38,6 +40,8 @@ namespace MusicFileManager
                 regKey = Registry.CurrentUser.CreateSubKey(regKeyLocation, RegistryKeyPermissionCheck.ReadWriteSubTree);
                 regKey.SetValue(regKeySearch, AppDomain.CurrentDomain.BaseDirectory);
             }
+
+            searchLocation = regKey.GetValue(regKeySearch) as string;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -47,7 +51,7 @@ namespace MusicFileManager
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            lblLocation.Content = regKey.GetValue(regKeySearch);
+            txbLocation.Text = searchLocation;
         }
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
@@ -68,13 +72,95 @@ namespace MusicFileManager
             if (result == System.Windows.Forms.DialogResult.OK)
             {
                 searchLocation = fd.SelectedPath;
-                lblLocation.Content = fd.SelectedPath;
+                txbLocation.Text = fd.SelectedPath;
             }
         }
 
         private void btnClean_Click(object sender, RoutedEventArgs e)
         {
+            cleanUp();
             regKey.SetValue(regKeySearch, searchLocation);
-        }  
+        }
+
+        private string[] getFiles(string location)
+        {
+            List<string> allFiles = new List<string>();
+            string[] allPaths = Directory.GetFiles(searchLocation, "*.*", SearchOption.AllDirectories);
+            foreach (string s in allPaths)
+            {
+                FileAttributes attr = File.GetAttributes(s);
+                if (!attr.HasFlag(FileAttributes.Directory))
+                {
+                    allFiles.Add(s);
+                }
+            }
+            return allFiles.ToArray();
+        }
+
+        private void cleanUp()
+        {
+            //프로그레스바 초기화
+            prgCleanUp.Minimum = 0;
+            prgCleanUp.Maximum = 100;
+
+            //백그라운드 작업자 초기화
+            BackgroundWorker worker = new BackgroundWorker();
+
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+            worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
+
+            worker.RunWorkerAsync();
+            
+        }
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            prgCleanUp.Value = e.ProgressPercentage;            
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Cancelled");
+            }
+            else
+            {
+                MessageBox.Show("Completed");
+            }
+
+            if (e.Error != null)
+            {
+                
+            }            
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            BackgroundWorker worker = sender as BackgroundWorker;
+            string[] files = getFiles(searchLocation);
+            
+            for (int i = 0; i < files.Count(); i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                if (ZipFile.IsZipFile(files[i]))
+                {
+                    MessageBox.Show(files[i] + " is zip file");
+                }
+                int perc = (int)((float)(i+1) / (float)files.Count() * 100);
+                worker.ReportProgress(perc);
+            }
+            
+        }
     }
 }
